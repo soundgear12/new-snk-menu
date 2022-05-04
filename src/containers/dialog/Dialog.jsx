e3634afd8e23d977fda6c87d98c150fb6d077f92
Antonio <antonio.rodriguez024@gmail.com>
2022-05-03 23:36:37 -0500

    added basic dialog box template

--- src/containers/dialog/Dialog.jsx
+++ src/containers/dialog/Dialog.jsx
@@ -3,8 +3,15 @@ import './dialog.css';
 
 const Dialog = () => {
   return (
-    <div>
-        
+    <div className='snk__dialog section__padding'>
+      <div className='snk__dialog-container'>
+        <div className='snk__dialog-container_dialog-box'>
+          <p>Play a perfect conversion of the arcade<br></br>game here.</p>
+        </div>
+      </div>
+      <div className='snk__dialog-container__footer'>
+        <p>MODE SELECT</p>
+      </div>
     </div>
   )
 }
